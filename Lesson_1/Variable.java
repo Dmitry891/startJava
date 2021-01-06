c488d572c2b474e2cbd62665defcb4967767560a
Dmitry891 <fda891via@gmail.com>
2021-01-06 19:15:54 +0300

    HW Variable change_1

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -2,13 +2,14 @@ public class Variable {
     public static void main(String[] args) {
         //Ниже приведены характеристики моего текущего персонального компьютера
         byte numberOfСpuCores = 2;
-        short CpuFrequency = 3200;
+        short сpuFrequency = 3200;
         int sizeOfRam = 800000000;
-        long RamFrequency= 160000000000L; 
+        long ramFrequency = 160000000000L; 
         float totalStorageCapacity = 476.94f;
         double freeStorageCapacity = 268.96;
         boolean windowsSystеm = true;
         char finish = '\u0021';
+
         System.out.println("Specifications");
         System.out.println("Number of CPU Cores:" + "                     " + numberOfСpuCores);
         System.out.println("CPU Frequency, MHz:" + "                   " + CpuFrequency);
