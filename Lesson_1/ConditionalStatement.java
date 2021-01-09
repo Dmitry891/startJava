270c562f029eae2b42dcdb1ef9cbf48d6656c3be
Dmitry891 <fda891via@gmail.com>
2021-01-10 00:00:52 +0300

    HW Calculator change2

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,33 +1,29 @@
 public class ConditionalStatement {   
     public static void main(String[] args) {
         byte age = 31;
+        boolean maleGender = true;
+        float height = 1.77f;
+        char firstLetterOfName = 'D';
         if (age > 20) { 
             System.out.println("Age is more than 20 years");
-        }
-        
-        boolean maleGender = true;
-        if (maleGender) {
+        } 
+        if (maleGender == true) {
             System.out.println("Person is a man");
         }
-        if (!maleGender) {
+        if (maleGender != true) {
             System.out.println("Person is a woman");
         }
-        
-        float height = 1.77f;
         if (height < 1.80) {
             System.out.println("Height is less than 1.80");
         } else {
             System.out.println("Height is more than 1.80");
         }
-        
-        char firstLetterOfName = 'D';
         if (firstLetterOfName == 'M') {
             System.out.println("M is not the first letter of a name");
         } else if (firstLetterOfName == 'I') {
             System.out.println("I is not the first letter of a name");
         } else {
             System.out.println("The first letter of a name is D");
-
         }
     }
 }
