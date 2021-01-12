45fc3a47dfaaebc2d696f6dc1e652e1bb491c44d
Dmitry891 <fda891via@gmail.com>
2021-01-12 19:53:29 +0300

    HW Calculator Lesson_2

---
+++ Lesson_2/Calculator.java
@@ -0,0 +1,50 @@
+public class Calculator {   
+
+    private int firstNumber;
+    private int secondNumber;
+    private char operation;
+    private int result;
+
+    void setFirstNumber(int firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    void setSecondNumber(int secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    void setOperation(char operation) {
+        this.operation = operation;
+    }
+
+    public int getResult() {
+        switch(operation) {
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            case '/':
+                result = firstNumber / secondNumber;
+                break;
+            case '%':
+                result = firstNumber % secondNumber;
+                break;
+            case '^':
+                result = 1;
+                    for (int i = 1; i <= secondNumber; i++) {
+                        result *= firstNumber;
+                    }
+                break;
+            default:
+                System.out.println("You've entered an invalid operation! Try again.");
+                break;
+        }
+         System.out.println("Result is " + result);
+         return result;
+     }
+}
---
+++ Lesson_2/CalculatorTest.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator CalculatorSimple = new Calculator ();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a first number: ");
+        int firstNumber = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter an operation (+, -, *, /, %, ^): ");
+        char operation = scanner.next().charAt(0);
+        scanner.nextLine();
+        System.out.print("Enter a second number: ");
+        int secondNumber = scanner.nextInt();
+        scanner.nextLine();
+        CalculatorSimple.setFirstNumber(firstNumber);
+        CalculatorSimple.setOperation(operation);
+        CalculatorSimple.setSecondNumber(secondNumber);
+        CalculatorSimple.getResult();
+        System.out.println("Do you want to continue? [yes/no]");
+        String answer = scanner.nextLine();
+            switch(answer) {
+                case "yes":
+                    System.out.println("Enter a first number: ");
+                    break;
+                case "no":
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Do you want to continue? [yes/no]");
+                    break;
+            }
+    }
+}
